adda9807a83974286e50432d4fb212bfae0bcc6f
Vladyslav Rybak <rubakvlad7@gmail.com>
2024-07-09 21:05:53 +0300

    Change a structure

--- js/script.js
+++ js/script.js
@@ -1,253 +1,160 @@
+"use strict";
+
 // Header variables
-let burger = document.querySelector(".header__burger");
-let menu = document.querySelector(".header__menu");
-let list = document.querySelector(".header__list");
-let headerName = document.querySelector(".header__name");
-let moveLock = document.querySelector("body");
-
-// Gallery variables
-let galleryImages = document.querySelectorAll(".gallery__img");
-let getLatestOpenedImg;
-let windowWidth = window.innerWidth;
-
-// Interier variables
-let interierImages = document.querySelectorAll(".interier__img");
+const burger = document.querySelector(".header-burger");
+const menu = document.querySelector(".header-menu");
+const list = document.querySelector(".header-list");
+const headerName = document.querySelector(".header-name");
+const moveLock = document.querySelector("body");
 
 // Close menu on icon
 burger.onclick = function () {
-	burger.classList.toggle("active");
-	menu.classList.toggle("active");
-	moveLock.classList.toggle("lock");
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+  moveLock.classList.toggle("lock");
 };
 
 list.onclick = function () {
-	list.classList.remove("active");
-	moveLock.classList.toggle("lock");
+  list.classList.remove("active");
+  moveLock.classList.toggle("lock");
 };
 
 headerName.onclick = function () {
-	moveLock.classList.remove("lock");
+  moveLock.classList.remove("lock");
 };
 
 // Close menu on link click
-document.querySelectorAll(".header__name, .header__link").forEach((n) =>
-	n.addEventListener("click", () => {
-		burger.classList.remove("active");
-		menu.classList.remove("active");
-	})
+document.querySelectorAll(".header-name, .header-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    burger.classList.remove("active");
+    menu.classList.remove("active");
+  })
 );
 
 // Biography readmore button
-var i = 0;
+let i = 0;
+
 function readmore() {
-	if (!i) {
-		document.querySelector(".biography__moretext").style.display = "inline";
-		document.querySelector(".biography__button").innerHTML = "Згорнути";
-		i = 1;
-	} else {
-		document.querySelector(".biography__moretext").style.display = "none";
-		document.querySelector(".biography__button").innerHTML = "Дізнатись більше";
-		i = 0;
-	}
+  if (!i) {
+    document.querySelector(".biography-more-text").style.display = "inline";
+    document.querySelector(".biography-button").innerHTML = "Згорнути";
+    i = 1;
+  } else {
+    document.querySelector(".biography-more-text").style.display = "none";
+    document.querySelector(".biography-button").innerHTML = "Дізнатись більше";
+    i = 0;
+  }
 }
 
-// Gallery Images
-if (galleryImages) {
-	// Create onclick function for each image
-	galleryImages.forEach(function (image, index) {
-		image.onclick = function () {
-			// Get the image URL from our element CSS
-			let getElementCss = window.getComputedStyle(image);
-			let getFullImgUrl = getElementCss.getPropertyValue("background-image");
-			let getImgUrlPos = getFullImgUrl.split("/img/gallery/");
-			let setNewImgUrl = getImgUrlPos[1].replace('")', "");
+// Form
 
-			// Save the image nr to use later with prev/next buttons
-			getLatestOpenedImg = index + 1;
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("form");
 
-			// Create a popup window
-			let container = document.body;
-			let newImgWindow = document.createElement("div");
-			container.appendChild(newImgWindow);
-			newImgWindow.setAttribute("class", "img-window");
-			newImgWindow.setAttribute("onclick", "closeImg()");
-			document.addEventListener("keyup", function (event) {
-				// Close the window on `Escape` key press
-				if (event.code === "Escape") {
-					closeImg();
-				}
-			});
+//   form.addEventListener("submit", formSend);
 
-			// Insert image inside window
-			let newImg = document.createElement("img");
-			newImgWindow.appendChild(newImg);
-			newImg.setAttribute("src", "img/gallery/" + setNewImgUrl);
-			newImg.setAttribute("id", "current-image");
+//   async function formSend(e) {
+//     e.preventDefault();
 
-			// Prev/Next buttons
-			newImg.onload = function () {
-				let imgWidth = this.width;
-				let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
+//     let error = formValidate(form);
 
-				let newNextBtn = document.createElement("a");
-				let btnNextText = document.createTextNode(">");
-				newNextBtn.appendChild(btnNextText);
-				container.appendChild(newNextBtn);
-				newNextBtn.setAttribute("class", "img-btn-next");
-				newNextBtn.setAttribute("onclick", "changeImg(1)");
-				newNextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
+//     let formData = new FormData(form);
+//     formData.append("image", formImage.files[0]);
 
-				let newPrevBtn = document.createElement("a");
-				let btnPrevText = document.createTextNode("<");
-				newPrevBtn.appendChild(btnPrevText);
-				container.appendChild(newPrevBtn);
-				newPrevBtn.setAttribute("class", "img-btn-prev");
-				newPrevBtn.setAttribute("onclick", "changeImg(0)");
-				newPrevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
-			};
-		};
-	});
-}
+//     if (error === 0) {
+//       form.classList.add("_sending");
+//       let response = await fetch("sendmail.php", {
+//         method: "POST",
+//         body: formData,
+//       });
 
-function closeImg() {
-	// Remove the image window
-	document.querySelector(".img-window").remove();
+//       if (response.ok) {
+//         let result = await response.json();
+//         alert(result.message);
+//         formPreview.innerHTML = "";
+//         form.reset();
+//         form.classList.remove("_sending");
+//       } else {
+//         alert("Error");
+//         form.classList.remove("_sending");
+//       }
+//     } else {
+//       alert("Fill in the required fields");
+//     }
+//   }
 
-	// Remove the prev/next buttons
-	document.querySelector(".img-btn-next").remove();
-	document.querySelector(".img-btn-prev").remove();
-}
+//   function formValidate(form) {
+//     let error = 0;
+//     let formReq = document.querySelectorAll("._req");
 
-function changeImg(changeDir) {
-	// Remove current image
-	document.querySelector("#current-image").remove();
+//     for (let index = 0; index < formReq.length; index++) {
+//       const input = formReq[index];
+//       formRemoveError(input);
 
-	// Generate new image
-	let getImgWindow = document.querySelector(".img-window");
-	let newImg = document.createElement("img");
-	getImgWindow.appendChild(newImg);
+//       if (input.classList.contains("_email")) {
+//         if (emailTest(input)) {
+//           formAddError(input);
+//           error++;
+//         }
+//       } else if (
+//         input.getAttribute("type") === "checkbox" &&
+//         input.checked === false
+//       ) {
+//         formAddError(input);
+//         error++;
+//       } else {
+//         if (input.value === "") {
+//           formAddError(input);
+//           error++;
+//         }
+//       }
+//     }
+//     return error;
+//   }
 
-	// Set new image "src"
-	let calcNewImg;
-	if (changeDir === 1) {
-		calcNewImg = getLatestOpenedImg + 1;
-		if (calcNewImg > galleryImages.length) {
-			calcNewImg = 1;
-		}
-	} else if (changeDir === 0) {
-		calcNewImg = getLatestOpenedImg - 1;
-		if (calcNewImg < 1) {
-			calcNewImg = galleryImages.length;
-		}
-	}
-	newImg.setAttribute("src", "img/gallery/img_" + calcNewImg + ".jpg");
-	newImg.setAttribute("id", "current-image");
+//   function formAddError(input) {
+//     input.parentElement.classList.add("_error");
+//     input.classList.add("_error");
+//   }
 
-	// Adjust our global variable "getLatestOpenedImg"
-	getLatestOpenedImg = calcNewImg;
+//   function formRemoveError(input) {
+//     input.parentElement.classList.remove("_error");
+//     input.classList.remove("_error");
+//   }
 
-	// Change the button positions
-	newImg.onload = function () {
-		let imgWidth = this.width;
-		let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
+//   function emailTest(input) {
+//     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//   }
 
-		let nextBtn = document.querySelector(".img-btn-next");
-		nextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
+//   const formImage = document.getElementById("formImage");
+//   const formPreview = document.getElementById("formPreview");
 
-		let prevBtn = document.querySelector(".img-btn-prev");
-		prevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
-	};
-}
+//   formImage.addEventListener("change", () => {
+//     uploadFile(formImage.files[0]);
+//   });
 
-// Interier Images
-if (interierImages) {
-	// Create onclick function for each image
-	interierImages.forEach(function (image, index) {
-		image.onclick = function () {
-			// Get the image URL from our element CSS
-			let getElementCss = window.getComputedStyle(image);
-			let getFullImgUrl = getElementCss.getPropertyValue("background-image");
-			let getImgUrlPos = getFullImgUrl.split("/img/interier/");
-			let setNewImgUrl = getImgUrlPos[1].replace('")', "");
+//   function uploadFile(file) {
+//     if (!["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
+//       alert("Only images are allowed.");
+//       formImage.value = "";
+//       return;
+//     }
 
-			// Save the image nr to use later with prev/next buttons
-			getLatestOpenedImg = index + 1;
+//     if (file.size > 2 * 1024 * 1024) {
+//       alert("The file must be less than 2 MB.");
+//       return;
+//     }
 
-			// Create a popup window
-			let container = document.body;
-			let newImgWindow = document.createElement("div");
-			container.appendChild(newImgWindow);
-			newImgWindow.setAttribute("class", "img-window");
-			newImgWindow.setAttribute("onclick", "closeImg()");
+//     var reader = new FileReader();
 
-			// Insert image inside window
-			let newImg = document.createElement("img");
-			newImgWindow.appendChild(newImg);
-			newImg.setAttribute("src", "img/interier/" + setNewImgUrl);
-			newImg.setAttribute("id", "current-image");
+//     reader.onload = function (e) {
+//       formPreview.innerHTML = `<img src="${e.target.result}" alt="Photo">`;
+//     };
 
-			// Prev/Next buttons
-			newImg.onload = function () {
-				let imgWidth = this.width;
-				let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
+//     reader.onerror = function (e) {
+//       alert("Error");
+//     };
 
-				let newNextBtn = document.createElement("a");
-				let btnNextText = document.createTextNode(">");
-				newNextBtn.appendChild(btnNextText);
-				container.appendChild(newNextBtn);
-				newNextBtn.setAttribute("class", "img-btn-next");
-				newNextBtn.setAttribute("onclick", "changeImgInterier(1)");
-				newNextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
-
-				let newPrevBtn = document.createElement("a");
-				let btnPrevText = document.createTextNode("<");
-				newPrevBtn.appendChild(btnPrevText);
-				container.appendChild(newPrevBtn);
-				newPrevBtn.setAttribute("class", "img-btn-prev");
-				newPrevBtn.setAttribute("onclick", "changeImgInterier(0)");
-				newPrevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
-			};
-		};
-	});
-}
-
-function changeImgInterier(changeDir) {
-	// Remove current image
-	document.querySelector("#current-image").remove();
-
-	// Generate new image
-	let getImgWindow = document.querySelector(".img-window");
-	let newImg = document.createElement("img");
-	getImgWindow.appendChild(newImg);
-
-	// Set new image "src"
-	let calcNewImg;
-	if (changeDir === 1) {
-		calcNewImg = getLatestOpenedImg + 1;
-		if (calcNewImg > interierImages.length) {
-			calcNewImg = 1;
-		}
-	} else if (changeDir === 0) {
-		calcNewImg = getLatestOpenedImg - 1;
-		if (calcNewImg < 1) {
-			calcNewImg = interierImages.length;
-		}
-	}
-	newImg.setAttribute("src", "img/interier/img_" + calcNewImg + ".png");
-	newImg.setAttribute("id", "current-image");
-
-	// Adjust our global variable "getLatestOpenedImg"
-	getLatestOpenedImg = calcNewImg;
-
-	// Change the button positions
-	newImg.onload = function () {
-		let imgWidth = this.width;
-		let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
-
-		let nextBtn = document.querySelector(".img-btn-next");
-		nextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
-
-		let prevBtn = document.querySelector(".img-btn-prev");
-		prevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
-	};
-}
+//     reader.readAsDataURL(file);
+//   }
+// });
